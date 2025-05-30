c0da41c6e151b2ccaeca84a413806509d6ada7db
ananda17gb <ananda.widigdo17@gmail.com>
2025-05-30 23:55:34 +0700

    fix: fixing typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SignIn from './page/SignIn';
-import SignUp from './page/SignUp';
-import Home from './page/homepage/Home';
-import PetDetail from './page/homepage/PetDetail';
-import Listing from './page/homepage/listing';
-import Community from './page/homepage/community';
-import MyHub from './page/homepage/PetHub';
-import PostCreationPage from './page/homepage/CreatePost';
-import CreateAdoption from './page/homepage/AdoptionPost';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./page/SignIn";
+import SignUp from "./page/SignUp";
+import Home from "./page/homepage/Home";
+import PetDetail from "./page/homepage/PetDetail";
+import Listing from "./page/homepage/Listing";
+import Community from "./page/homepage/Community";
+import MyHub from "./page/homepage/PetHub";
+import PostCreationPage from "./page/homepage/CreatePost";
+import CreateAdoption from "./page/homepage/AdoptionPost";
 
 function App() {
   return (
@@ -16,14 +15,13 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/listing" element={<Listing/>} />
-        <Route path="/petdetail" element={<PetDetail/>} />
-        <Route path="/community" element={<Community/>} />
-        <Route path="/myhub" element={<MyHub/>} />
-        <Route path="/createadopt" element={<CreateAdoption/>} />
-        <Route path="/createpost" element={<PostCreationPage/>} />
-        
+        <Route path="/home" element={<Home />} />
+        <Route path="/listing" element={<Listing />} />
+        <Route path="/petdetail" element={<PetDetail />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/myhub" element={<MyHub />} />
+        <Route path="/createadopt" element={<CreateAdoption />} />
+        <Route path="/createpost" element={<PostCreationPage />} />
       </Routes>
     </Router>
   );
